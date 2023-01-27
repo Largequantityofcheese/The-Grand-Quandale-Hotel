14e80ed5929a05b8192ea745edb35dfc984bfd40
Largequantityofcheese <122464891+Largequantityofcheese@users.noreply.github.com>
2023-01-27 10:34:20 -0500

    holduop
    ring ding ding ding ding ding ding

--- clients.py
+++ clients.py
@@ -1,1 +1,28 @@
 #hey buddy make this code work
+
+class client():
+  def __init__(self):
+    name
+    assignedRoom
+    contactInformation
+    securityDeposit
+    checkOutPayment
+    self.hotelGiftBalance=0
+    dietaryRestrictions
+    timeOfCheckIn
+    clientstatus
+    
+  def Inputnameandinfo(self):
+    
+  def checkClientStatus():
+  def sellroom():
+  def redeemGiftCard(self):
+      self.hotelGiftBalance+=50
+      
+  def Refusecustomer():
+   
+def main():
+    Inputnameandinfo()
+    checkClientStatus()
+    sellroom()
+    #please stop saving pdfs in our database
