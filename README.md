da08a96c1b4bdf32f3708c6c6c0555798c600620
Largequantityofcheese <122464891+Largequantityofcheese@users.noreply.github.com>
2023-01-31 09:57:53 -0500

    re-added stuff, FIXED(kinda)\

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # The-Grand-Quandale-Hotel
 Hotel management system ran by jamacian gangsters with leprosy and sickle cell
+deleted rooms because it is NOTHING.
--- mainframe.py
+++ mainframe.py
@@ -1,56 +1,7 @@
-#hey buddy make this code work
-banList=["Joe Biden","Quandale Dingle"]
-
-class client():
-  def __init__(self):
-    self.name=None
-    self.assignedRoom=0
-    self.contactInformation=None
-    self.securityDeposit=0
-    self.checkOutPayment=0
-    self.hotelGiftBalance=0
-    self.dietaryRestrictions=None
-    self.timeOfCheckIn=None
-    self.clientstatus="Respectable"
-    
-  def Inputnameandinfo(self):
-    while True:
-      self.name=input("please type your name: ")
-      answer=input(f"{self.name}, is this your correct first and last name?")
-      if "yes" in answer or "y" in answer:
-        self.contactInformation=input("please input your phone number: ")
-        response=input(f"{self.contactInformation}, is this your correct phone number?")
-        if "yes" in answer or "y" in answer:
-          break
-
-  def checkClientStatus(self):
-    if self.name in banList:
-      self.clientstatus="Banned"
-      print("You are not allowed in this establishment, security will be with you shortly.")
-
-  def sellroom():
-    return None
-
-  def redeemGiftCard(self):
-      self.hotelGiftBalance+=50
-      
-  def Refusecustomer():
-   return None
-
-def main():
-    Inputnameandinfo()
-    checkClientStatus()
-    sellroom()
-    #please stop saving pdfs in our database\
-
-main()
-
 class mainframe:
     def __init__(self):
         #setting up individual room costs
         self.roomHierarchyClass={"Economy":50,"Economy Plus":100,"Suite":200}
-        
-
 
     def createDbMainframe():
         frame=[]
@@ -119,5 +70,8 @@ def main():
     createDbMainframe()
     determineRoomQuality(self)
     addChargeToAmtOwed(self,total,c)
+    Inputnameandinfo()
+    checkClientStatus()
+    sellroom()
 
 main()
